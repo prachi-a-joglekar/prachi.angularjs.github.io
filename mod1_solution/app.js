39c33cf01c0a8164f3678e0c97ed2195bf6573c8
502744868 <prachi.joglekar@ge.com>
2020-08-11 15:49:03 +0530

    color changes

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -8,23 +8,31 @@
         
         $scope.checkIfTooMuch = function(){
            console.log($scope.lunchmenu);
+           $scope.customStyle = {};
            $scope.result="";
            var lunchmenu  = $scope.lunchmenu;
            if (undefined == lunchmenu || lunchmenu == ""){
             $scope.result = "Please enter data first";
+            $scope.customStyle.colorClass = "red";
+
            }else{
+                
                 var menuItems = lunchmenu.split(',');
 
                 var filtered = menuItems.filter(function (el) {
                     return el != null && el != '' && el != undefined;
                   });
-                  
-                
-                 var len =  filtered.length;
-                if (len<=3){
+                                  
+                var len =  filtered.length;
+                if (len==0){
+                    $scope.result = "Please enter data first";
+                    $scope.customStyle.colorClass = "red";
+                }else if (len<=3){
                     $scope.result = "Enjoy";
+                    $scope.customStyle.colorClass = "green";
                 }else{
                     $scope.result = "Too much!";
+                    $scope.customStyle.colorClass = "green";
                 }
             }
 
